d1f38a1b7d937036bf6a33b1fe2ba311c3ecc506
Huib Aldewereld <huib@aldewereld.com>
2014-02-25 13:08:20 +0100

    Adding empty implementation of ChannelParser

---
+++ src/nl/tudelft/tbm/pvr/util/ChannelParser.java
@@ -0,0 +1,16 @@
+package nl.tudelft.tbm.pvr.util;
+
+import java.util.ArrayList;
+
+import nl.tudelft.tbm.pvr.data.Channel;
+
+/**
+ * @author student
+ */
+public class ChannelParser implements ChannelParserInterface {
+
+    public ArrayList<Channel> createChannels() {
+        System.err.println("Channel parser not yet implemented!");
+        return new ArrayList<Channel>();
+    }
+}
