1489726c12d9067dc43bf3f6f356301536657cb3
Huib Aldewereld <huib@aldewereld.com>
2014-02-25 13:08:06 +0100

    Adding ChannelParserInterface

---
+++ src/nl/tudelft/tbm/pvr/util/ChannelParserInterface.java
@@ -0,0 +1,13 @@
+package nl.tudelft.tbm.pvr.util;
+
+import java.util.ArrayList;
+
+import nl.tudelft.tbm.pvr.data.Channel;
+
+/**
+ * @author Huib Aldewereld
+ */
+public interface ChannelParserInterface {
+
+    public ArrayList<Channel> createChannels();
+}
